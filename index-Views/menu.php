b513aac398b6f92fc8318442808c080c10c77581
LucianoEun <lucianoeun@hotmail.com>
2016-11-21 18:55:37 -0200

    Trabalhos no index
    criaçao da base menu,home, css

---
+++ index-Views/menu.php
@@ -0,0 +1,3 @@
+<?php
+ echo"Menu page do index";
+ ?>
--- index.php
+++ index.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Trilhas Adventure</title>
+   <link href="./index-Views/Css/index.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
-
+    <div class="box-full">
+              <div class="box-center">
+                   <?php include_once './index-Views/menu.php'; ?>
+              </div>
+    </div>
   </body>
 </html>
