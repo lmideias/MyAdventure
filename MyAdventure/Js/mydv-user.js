2b69b5280af0ea01422e02a0b685b35532cfef56
LucianoEun <lucianoeun@hotmail.com>
2016-12-02 15:13:27 -0200

    inicio do angular
    annlar no pc eipanema

---
+++ MyAdventure/Js/mydv-user.js
@@ -0,0 +1,5 @@
+var myApp = angular.module("",[]);
+myapp.controller("caduserCtrl", function($scope){
+  $scope.nome = "Olá mundo";
+
+});
---
+++ MyAdventure/Views/Caduser.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html ng-app="myapp">
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastrar usuario</title>
+
+  </head>
+  <body>
+  <div class="" ng-controller="caduserCtrl">
+
+  <h3>{{nome}}</h3>
+
+  </div>
+
+  <script src="Js/myadv-user.js" ></script>
+  <script src="../Inc/Libs/angular-angular.js" ></script>
+  </body>
+</html>
