474f20df75e0918c75d3fb56064bda9bcbd354ae
LucianoEun <lucianoeun@hotmail.com>
2016-12-02 23:01:11 -0200

    criaçao do user css
    css em casa

--- MyAdventure/index.php
+++ MyAdventure/index.php
@@ -4,9 +4,24 @@
     <meta charset="utf-8">
     <title>MyAdventure</title>
      <link href="Css/myadv-mobile.css" rel="stylesheet" type="text/css"/>
+      <link href="Css/myadv-desktop.css" rel="stylesheet" type="text/css"/>
       <link href="../Inc/Libs/reset.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
+   <div class="barra-full">
+      <div class="barra-full-center">
+        <div class="bx-1">
+
+        </div>
+        <div class="bx-2">
+
+        </div>
+        <div class="bx-3">
+          <h4>Sair</h4>
+        </div>
+     </div>
+   </div>
+
     <div class="body-container">
       <div class="a-container">
         <h1 class="">Olá</h1>
