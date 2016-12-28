b2512be12cc5f529a5e9773514b19748ae8f5fb7
LucianoEun <lucianoeun@hotmail.com>
2016-12-28 19:57:58 -0200

    Criaçao do myadv pagina
    no pc de ipanema

---
+++ MyAdventure/MyAdv.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+    <title>MyAdventure</title>
+       <link href="Css/MyAdv.css" rel="stylesheet" type="text/css"/>
+       <link href="../Inc/Libs/reset.css" rel="stylesheet" type="text/css"/>
+       <link href="../Inc/Libs/bootstrap.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+<div class="lmi-head">
+
+  <div class="lmi-head-center">
+
+  </div>
+
+</div>
+
+<div class="lmi-head-prop">
+       <h5>Propaganda central do adventure</h5>
+</div>
+
+<div class="lmi-body">
+  <div class="lmi-body-center">
+    <div class="lmi-body-center-left">
+
+    </div>
+    <div class="lmi-body-center-center">
+
+    </div>
+    <div class="lmi-body-center-right">
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+<div class="lmi-footer">
+  <div class="lmi-footer-center">
+  </div>
+</div>
+  </body>
+</html>
