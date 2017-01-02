4026a2fabc4c3277e5f9c839395ee26f8263465b
LucianoEun <lucianoeun@hotmail.com>
2017-01-02 20:26:45 -0200

    Pc Ipanema
    melhorias nas páginas principais

---
+++ Artigos/Views/tMenu.php
@@ -0,0 +1,15 @@
+<div class="container">
+<div class="boxMenu">
+  <h1>Dicas</h1>
+</div>
+<div class="boxMenu">
+  <h1>Livre</h1>
+</div>
+<div class="boxMenu">
+  <h1>Trilhas</h1>
+</div>
+<div class="boxMenu">
+  <h1>Sobre</h1>
+</div>
+
+</div>
--- Artigos/index.php
+++ Artigos/index.php
@@ -2,9 +2,47 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>artigos</title>
+    <title>Trilhas Adventure</title>
+   <link href="Css/artigos.css" rel="stylesheet" type="text/css"/>
+   <link href="../Inc/Libs/reset.css" rel="stylesheet" type="text/css"/>
+
   </head>
-  <body>
+  <body class="bodyC">
+    <div class="box-index-full">
+              <div class="box-index-center">
+
+                <div class="participar">
+                  Participar / menu / opções/ outros / sair
+                </div>
+
+              </div>
+    </div>
+    <div class="box-index-body">
+
+
+      <div class="box-index-body-left">
+
+      <?php include "Views/tMenu.php"; ?>
+
+      </div>
+      <div class="box-index-body-right">
+        <div class="prop1">
+          <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ <!-- PropResponse -->
+ <ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-5664434222802350"
+    data-ad-slot="9520149548"
+    data-ad-format="auto"></ins>
+ <script>
+ (adsbygoogle = window.adsbygoogle || []).push({});
+ </script>
+        </div>
+      </div>
+
+
+
+    </div>
 
   </body>
 </html>
