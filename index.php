3fea3785ff7f14163cb7ef99234727753f074ade
LucianoEun <lucianoeun@hotmail.com>
2017-01-10 18:26:33 -0200

    pc ipanema, melhorias na home-index
    header, botao e divisao de tela

--- index.php
+++ index.php
@@ -9,12 +9,31 @@
   </head>
   <body>
     <div class="box-index-full">
+          <div class="box-index-full-linha">
+          </div>
               <div class="box-index-center">
-
-                <div class="participar">
-                <a href="ViewsForms/login.php">Participar</a>
+                <div class="box-index-center-left">
+  <h1>caixa esquerda</h1>
                 </div>
-                        <h1>Index</h1>
+                <div class="box-index-center-center">
+                  <div class="participar">
+                     <h4>Participar</h4>
+                  </div>
+                </div>
+                <div class="box-index-center-right">
+                       <div class="">
+                          <!-- Widget Previs&atilde;o de Tempo CPTEC/INPE -->
+                          <iframe allowtransparency="true" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" src="http://www.cptec.inpe.br/widget/widget.php?p=null&w=h&c=f3f3f3&f=423f42" height="200px" width="215px"></iframe>
+                          <noscript>Previs&atilde;o de <a href="http://www.cptec.inpe.br/cidades/tempo/null"></a> oferecido por <a href="http://www.cptec.inpe.br">CPTEC/INPE</a></noscript>
+                          <!-- Widget Previs&atilde;o de Tempo CPTEC/INPE -->
+
+                       </div>
+                </div>
+
+
+
+
+
               </div>
     </div>
     <div class="box-index-body">
