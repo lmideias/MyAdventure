10be34fd04709ff0ae47e64d29b7faea71758331
LucianoEun <lucianoeun@hotmail.com>
2017-01-12 17:33:15 -0200

    PC ipanema, criação do css mobile
    css mobile na pagina adm

---
+++ Adm/mobile.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mobile Adm</title>
+   <link rel="stylesheet" href="Css/mobile.css" />
+   <link href="Css/Reset.css" rel="stylesheet" type="text/css"/>
+  <link href="../Inc/Libs/bootstrap.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+<div class="lmi-barra-full-linha">
+</div>
+<div class="lmi-barra-full">
+  <div class="lmi-barra-center">
+    <div class="lmi-menu-box">
+  Menu
+    </div>
+  </div>
+</div>
+
+
+
+<div class="box1">
+
+
+
+
+
+
+
+
+
+
+  <div class="base-a">
+    <div class="bx">
+     <h4>Cadastrar Usuário</h4>
+    </div>
+    <hr>
+    <form method="POST">
+   <div class="form-inline">
+     <div class="form-group">
+       <input type="text" class="form-control" name="celular" id="celular" placeholder="Celular">
+     </div>
+     <div class="form-group">
+       <input type="text" class="form-control" name="senha" id="senha" placeholder="Senha">
+     </div>
+   </div>
+
+   <div class="bx">
+     <!-- Indicates a successful or positive action -->
+     <input type="submit" class="btn btn-default" name="cadastrar" value="Cadastrar">
+
+   </div>
+
+  </form>
+  <div class="bx">
+    <p class="">
+      <a href="user.php">Sair</a>
+      <a href="/MyAdventure/">Voltar</a>
+    </p>
+  </div>
+
+  </div>
+</div>
+<div class="box2">
+  <h1>Box 01</h1>
+
+  <p>
+O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.
+O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500,
+quando uma misturou os caracteres de um texto para criar um espécime de livro.
+
+  </p>
+</div>
+  </body>
+</html>
