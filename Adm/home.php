34d6bb0249a3950c2caa5fc97c97bf86cf9d74fa
LucianoEun <lucianoeun@hotmail.com>
2017-01-26 00:25:45 -0200

    pc de casa
    trabalho no index principal

--- Adm/home.php
+++ Adm/home.php
@@ -6,6 +6,20 @@
    <link rel="stylesheet" href="Css/mobile.css" />
    <link href="Css/Reset.css" rel="stylesheet" type="text/css"/>
   <link href="../Inc/Libs/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+  <style type="text/css">
+  ul {list-style: none;padding: 0px;margin: 0px;}
+  ul li {display: block;position: relative;float: left;border:1px solid #000}
+  li ul {display: none;}
+  ul li a {display: block;background: #000;padding: 5px 10px 5px 10px;text-decoration: none;
+           white-space: nowrap;color: #fff;}
+  ul li a:hover {background: #f00;}
+  li:hover ul {display: block; position: absolute;}
+  li:hover li {float: none;}
+  li:hover a {background: #f00;}
+  li:hover li a:hover {background: #000;}
+  #drop-nav li ul li {border-top: 0px;}
+</style>
   </head>
   <body class="lmi-body-bg">
 <div class="lmi-barra-full-linha"></div>
@@ -15,6 +29,31 @@
   <div class="lmi-header-center">
     <div class="lmi-header-center-linha">
         <input type="submit" class="lmi-menu-participar" name="fechar" value=" Participar">
+        <ul id="drop-nav">
+          <li><a href="#">Support</a></li>
+          <li><a href="#">Web Design</a>
+            <ul>
+              <li><a href="#">HTML</a></li>
+              <li><a href="#">CSS</a></li>
+              <li><a href="#">JavaScript</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Content Management</a>
+            <ul>
+              <li><a href="#">Joomla</a></li>
+              <li><a href="#">Drupal</a></li>
+              <li><a href="#">WordPress</a></li>
+              <li><a href="#">Concrete 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contact</a>
+            <ul>
+              <li><a href="#">General Inquiries</a></li>
+              <li><a href="#">Ask me a Question</a></li>
+            </ul>
+          </li>
+        </ul>
+
 
     </div>
   </div>
--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="myApp">
   <head>
     <meta charset="utf-8">
-    <title>TA</title>
+    <title>Mobile Adm</title>
+
+   <link rel="stylesheet" href="Css/mobile.css" />
+   <link href="Inc/Libs/reset.css" rel="stylesheet" type="text/css"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <link href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister" rel="stylesheet">
   </head>
-  <body>
-    <h1>Em construção</h1>
+  <body class="lmi-body-bg">
+    <div class="box-full-linha"> </div>
+
+
+<div class="box-full">
+  <div class="box-center">
+           <a href="#"> <div class="box-menu">  <h4>Participar</h4> </div> </a>
+  </div>
+</div>
+
+
+
+
   </body>
 </html>
